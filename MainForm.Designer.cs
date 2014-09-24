dc29445b80e1b02fade575e9573217c524ff0c7d
Simon Klein <git@simonklein.de>
2014-09-24 16:00:34 +0200

    initial release

---
+++ MainForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Contacts2SQL
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSettings = new System.Windows.Forms.Button();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.buttonRead);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonSettings);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonConnect);
+            this.splitContainer1.Size = new System.Drawing.Size(850, 396);
+            this.splitContainer1.SplitterDistance = 357;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(850, 357);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(12, 6);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(133, 23);
+            this.buttonRead.TabIndex = 8;
+            this.buttonRead.Text = "Kontakte lesen";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 6;
+            // 
+            // buttonSettings
+            // 
+            this.buttonSettings.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSettings.Location = new System.Drawing.Point(753, 6);
+            this.buttonSettings.Name = "buttonSettings";
+            this.buttonSettings.Size = new System.Drawing.Size(85, 23);
+            this.buttonSettings.TabIndex = 5;
+            this.buttonSettings.Text = "Einstellungen";
+            this.buttonSettings.UseVisualStyleBackColor = true;
+            this.buttonSettings.Click += new System.EventHandler(this.buttonSettings_Click);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Enabled = false;
+            this.buttonConnect.Location = new System.Drawing.Point(151, 6);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(139, 23);
+            this.buttonConnect.TabIndex = 4;
+            this.buttonConnect.Text = "Daten abspeichern";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 396);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "MainForm";
+            this.Text = "Contacts2SQL";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonSettings;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonRead;
+
+
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
+using Microsoft.Office.Interop.Outlook;
+
+namespace Contacts2SQL
+{
+    public partial class MainForm : Form
+    {
+        DataTable EntryTable = new DataTable();
+
+        MySqlConnectionStringBuilder sqlconn = new MySqlConnectionStringBuilder();
+
+        public Microsoft.Office.Interop.Outlook.Application objOutlook = new Microsoft.Office.Interop.Outlook.Application();
+        public Microsoft.Office.Interop.Outlook._NameSpace objNS;
+        bool AutoMode = false;
+
+        public string[] Columns =
+        {
+            "Name",
+            "Vorname",
+            "Titel",
+            "Firma",
+            "Abteilung",
+            "Straße",
+            "PLZ",
+            "Ort",
+            "Telefon",
+            "Fax",
+            "E-Mail",
+        };
+
+        public MainForm()
+        {
+            if (Environment.GetCommandLineArgs().Contains("-auto"))
+            {
+                AutoMode = true;
+            }
+
+            InitializeComponent();
+            if (AutoMode) {
+                this.Hide();
+                this.SuspendLayout();
+            }
+
+            foreach (string tag in Columns)
+            {
+                EntryTable.Columns.Add(tag);
+            }
+
+            dataGridView1.DataSource = EntryTable;
+
+            objNS = objOutlook.Session;
+
+            if (AutoMode)
+            {
+                getContactData();
+                updateTable();
+                this.Dispose();
+                this.Close();
+                Environment.Exit(0);
+            }
+        }
+
+
+        int countCharNum(string source, char needle)
+        {
+            int count = 0;
+            foreach (char c in source)
+                if (c == needle) count++;
+            return count;
+        }
+
+        void MakeCol(string col, DataTable table)
+        {
+            if (!table.Columns.Contains(col))
+                table.Columns.Add(col);
+        }
+
+        void dumpSQL()
+        {
+
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            updateTable();
+        }
+
+        public void updateTable(bool noChange = false)
+        {
+            Process plink = null;
+
+            if (Properties.Settings.Default.SSHtunnel)
+            {
+                try
+                {
+                    sqlconn.Server = @"localhost";
+                    sqlconn.Port = uint.Parse(Properties.Settings.Default.SSHPort);
+
+                    ProcessStartInfo psi = new ProcessStartInfo(Properties.Settings.Default.plinkPath);
+
+                    psi.Arguments = "-ssh -l " + Properties.Settings.Default.SSHUser + " -L " + Properties.Settings.Default.port + ":localhost:" +
+                                    Properties.Settings.Default.SSHPort + " -pw " + Properties.Settings.Default.SSHPass + " " + Properties.Settings.Default.server;
+
+
+                    psi.RedirectStandardInput = true;
+                    psi.RedirectStandardOutput = true;
+                    psi.WindowStyle = ProcessWindowStyle.Hidden;
+                    psi.UseShellExecute = false;
+                    psi.CreateNoWindow = true;
+
+                    plink = Process.Start(psi);
+                    
+                }
+                catch
+                {
+                    MessageBox.Show("SSH-Tunnel konnte nicht aufgebaut werden");
+                    return;
+                }
+            }
+            else
+            {
+                sqlconn.Server = Properties.Settings.Default.server;
+                sqlconn.Port = Properties.Settings.Default.port;
+            }
+
+            sqlconn.UserID = Properties.Settings.Default.user;
+            sqlconn.Password = Properties.Settings.Default.password;
+            sqlconn.Database = Properties.Settings.Default.database;
+
+            string table = Properties.Settings.Default.table;
+            EntryTable.TableName = table;
+
+            using (MySqlConnection connection = new MySqlConnection(sqlconn.ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    if (!connection.Ping())
+                    {
+                        throw new System.Exception("No Connection to Server");
+                    }
+
+                    if (!noChange)
+                    {
+                        MySqlCommand del = new MySqlCommand(@"TRUNCATE TABLE " + sqlconn.Database + "." + table, connection);
+                        del.ExecuteNonQuery();
+
+                        MySqlDataAdapter adapter = new MySqlDataAdapter(@"SELECT * FROM " + sqlconn.Database + "." + table, connection);
+                        MySqlCommandBuilder builder = new MySqlCommandBuilder(adapter);
+                        adapter.ContinueUpdateOnError = true;
+
+                        adapter.Update(EntryTable);
+                    }
+                    if (!AutoMode) MessageBox.Show("Erfolgreich");
+                }
+                catch (System.Exception ex)
+                {
+                    string tmp = "";
+                    if (plink != null)
+                        tmp = plink.StandardOutput.ReadToEnd();
+                    MessageBox.Show(ex.Message.ToString() + tmp);
+                }
+            }
+
+            try
+            {
+                plink.StandardInput.WriteLine("exit");
+
+                plink.Kill();
+                plink.Dispose();
+            }
+            catch { }
+
+            //EntryTable.Clear();
+        }
+
+        private void buttonSettings_Click(object sender, EventArgs e)
+        {
+            settings setWin = new settings(this);
+            setWin.Show();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        private MAPIFolder getFolderFromPath(string Path, _NameSpace ns, MAPIFolder parent = null)
+        {
+            if (Path.StartsWith(@"\\"))
+                Path = Path.Substring(2);
+
+            if (Path.EndsWith(@"\"))
+                Path = Path.Remove(Path.Length - 1);
+
+
+
+
+            if (Path.Contains(@"\"))
+            {
+                MAPIFolder parentFolder;
+
+                string[] subFolders = Path.Split(new char[] { '\\' }, 2);
+
+                if (parent == null)
+                    parentFolder = ns.Folders[subFolders[0]];
+                else
+                    parentFolder = parent.Folders[subFolders[0]];
+
+                return getFolderFromPath(subFolders[1], ns, parentFolder);
+            }
+            else
+            {
+                if (parent == null)
+                {
+                    return ns.Folders[Path];
+
+                }
+                else
+                {
+                    return parent.Folders[Path];
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getContactData();
+            buttonConnect.Enabled = true;
+        }
+
+        private void getContactData()
+        {
+            MAPIFolder f;
+            Items itms;
+
+            try
+            {
+                f = getFolderFromPath(Properties.Settings.Default.outlookPath, objNS);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Fehler: Ordner konnte nicht geöffnet werden..." + Environment.NewLine + ex.Message);
+                return;
+            }
+
+            if (Properties.Settings.Default.locationFilter != "")
+            {
+                try
+                {
+                    itms = f.Items.Restrict(Properties.Settings.Default.locationFilter);
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show("Fehler: Filter konnte nicht angewendet werden..." + Environment.NewLine + ex.Message);
+                    return;
+                }
+            }
+            else
+            {
+                itms = f.Items;
+            }
+
+            this.Cursor = Cursors.WaitCursor;
+            EntryTable.BeginLoadData();
+
+            try
+            {
+                foreach (var tmp in itms)
+                {
+                    if (!(tmp is ContactItem)) continue;
+
+                    ContactItem cIt = (ContactItem)tmp;
+                    DataRow newRow = EntryTable.NewRow();
+
+                    newRow["Name"] = cIt.LastName;
+                    newRow["Vorname"] = cIt.FirstName;
+                    newRow["Titel"] = cIt.User2;
+                    newRow["Firma"] = cIt.CompanyName;
+                    newRow["Abteilung"] = cIt.Department;
+                    newRow["Strasse"] = cIt.BusinessAddressStreet;
+                    newRow["PLZ"] = cIt.BusinessAddressPostalCode;
+                    newRow["Ort"] = cIt.BusinessAddressCity;
+                    newRow["Telefon"] = cIt.BusinessTelephoneNumber;
+                    newRow["Fax"] = cIt.BusinessFaxNumber;
+                    newRow["E-Mail"] = cIt.Email1Address;
+
+                    EntryTable.Rows.Add(newRow);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            EntryTable.EndLoadData();
+            this.Cursor = Cursors.Default;
+        }
+
+        void OL_AdvancedSearchComplete(Search SearchObject)
+        {
+            MessageBox.Show("bla");
+        }
+
+    }
+
+}
